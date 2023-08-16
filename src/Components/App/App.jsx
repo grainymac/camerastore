e2eff07021a6d42c52d216c22402e524b2e36a35
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-08-16 06:26:15 -0500

    finished setting up App component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,32 +1,44 @@
-import { useState } from 'react'
-import './App.sass'
+import React from 'react'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+//Pages
+import Home from '../../pages/Home'
+import Products from '../../pages/Products'
+import ProductDetails from '../../pages/ProductDetails'
+import Search from '../../pages/Search'
 
+//Components
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+//Layout
+const Layout = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home/ > }, 
+      { path: '/products/:id', element: <Products/ > },
+      { path: '/product/:id', element: <ProductDetails/ > },
+      { path: '/search', element: <Search/ > },
+    ],
+  },
+])
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
